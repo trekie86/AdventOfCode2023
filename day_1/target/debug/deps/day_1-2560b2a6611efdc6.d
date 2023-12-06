fd095760860edd2a9d579d94a46b3c22bc28721c
Rob Wolinski <trekie86@gmail.com>
2023-12-05 22:37:16 -0700

    day 1 cleanup

--- day_1/target/debug/deps/day_1-2560b2a6611efdc6.d
+++
@@ -1,5 +0,0 @@
-/Volumes/workplace/AdventOfCode2023/day_1/target/debug/deps/day_1-2560b2a6611efdc6: src/main.rs
-
-/Volumes/workplace/AdventOfCode2023/day_1/target/debug/deps/day_1-2560b2a6611efdc6.d: src/main.rs
-
-src/main.rs:
